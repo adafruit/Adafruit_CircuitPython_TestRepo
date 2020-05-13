757f197e47687a21a2b56fc1fa50de589e76a056
dherrada <dylan.herrada@gmail.com>
2020-05-13 16:38:36 -0400

    Ran black

--- adafruit_testrepo.py
+++ adafruit_testrepo.py
@@ -52,7 +52,22 @@ class Test:
         self._test_value = "Untested"
 
     @staticmethod
-    def test(foo, bar, foobar, fake, variable, im, running, out, of, names, foobarfoo, thing, thing_as_well, also_thing):
+    def test(
+        foo,
+        bar,
+        foobar,
+        fake,
+        variable,
+        im,
+        running,
+        out,
+        of,
+        names,
+        foobarfoo,
+        thing,
+        thing_as_well,
+        also_thing,
+    ):
         """Test function."""
         while True:
             pass
